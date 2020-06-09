86c402d8ac5891ab1baa786de002ed419a6e4aad
Richard Hristov <richardhristov@gmail.com>
2020-06-09 15:19:52 +0300

    Add weighted search

--- web/assets/js/site.js
+++ web/assets/js/site.js
@@ -128,6 +128,28 @@ function getAdjacentNodes(u) {
     });
 }
 
+function getFormulaWeight(e, depth) {
+  return depth*0.8 + parseInt(e.data('weight'))*0.6;
+}
+
+function getAdjacentNodesAlt(u, depth) {
+  return cy.edges()
+    .filter(e => e.source().id() === u.id() || e.target().id() === u.id())
+    .map(e => {
+      if (e.source().id() === u.id()) {
+        return [e.target(), getFormulaWeight(e, depth),];
+      }
+      return [e.source(), getFormulaWeight(e, depth),];
+    });
+}
+
+function getConnectingEdges(u, v) {
+  return cy.edges()
+    .filter(e => 
+      (e.source().id() === u.id() && e.target().id() === v.id()) ||
+      (e.target().id() === u.id() && e.source().id() === v.id()));
+}
+
 function getNodeById(id) {
   return cy.nodes().filter(n => n.id() === id)[0];
 }
@@ -138,6 +160,7 @@ function clearColoring() {
   }
 }
 
+// -- Plain BFS
 $(document).on('click', '#js-btn-dfs', function(e) {
   clearColoring();
   const start = $('#js-input-search-start').val();
@@ -156,8 +179,11 @@ $(document).on('click', '#js-btn-dfs', function(e) {
     window.alert('The start or end node doesnt exist');
     return;
   }
+  window.startNode = startNode;
+  window.endNode = endNode;
 
   const visited = [];
+  let l = 1;
   const dfs = u => {
     u.data('coloring', 'visited');
     visited.push(u.id());
@@ -165,7 +191,7 @@ $(document).on('click', '#js-btn-dfs', function(e) {
     if (u === endNode) {
       console.log('DFS found end');
       u.data('coloring', 'path');
-      return true;
+      return 0;
     }
 
     const adj = getAdjacentNodes(u)
@@ -173,23 +199,25 @@ $(document).on('click', '#js-btn-dfs', function(e) {
       .sort((a, b) => a[1] - b[1]);
     for (const [v, weight,] of adj) {
       const result = dfs(v);
-      if (result) {
+      if (result >= 0) {
+        l += 1;
         u.data('coloring', 'path');
-        return result;
+        return result + weight;
       }
     }
 
-    return false;
+    return -1;
   };
 
   const result = dfs(startNode);
   if (result) {
-    window.alert('Path was found');
+    window.alert(`Path was found with total weight: ${result}, length: ${l}`);
     return;
   }
   window.alert('No path was found');
 });
 
+// -- Plain DFS
 $(document).on('click', '#js-btn-bfs', function(e) {
   clearColoring();
   const start = $('#js-input-search-start').val();
@@ -208,6 +236,8 @@ $(document).on('click', '#js-btn-bfs', function(e) {
     window.alert('The start or end node doesnt exist');
     return;
   }
+  window.startNode = startNode;
+  window.endNode = endNode;
 
   const visited = [];
   const path = {};
@@ -238,19 +268,111 @@ $(document).on('click', '#js-btn-bfs', function(e) {
       queue.push(v);
     }
   }
-  window.autism = path;
-  window.startNode = startNode;
-  window.endNode = endNode;
 
   if (found) {
+    let w = 0;
+    let l = 1;
     let u = endNode.id();
+    let prev = getNodeById(u);
     while (u !== startNode.id()) {
       const uu = getNodeById(u);
       uu.data('coloring', 'path');
+      const [e] = getConnectingEdges(uu, prev);
+      if (e) {
+        w = parseInt(e.data('weight')) + w;
+      }
       u = path[u];
+      prev = uu;
+      l += 1;
+    }
+    const [e] = getConnectingEdges(startNode, prev);
+    if (e) {
+      w = parseInt(e.data('weight')) + w;
     }
     startNode.data('coloring', 'path');
-    window.alert('Path was found');
+    window.alert(`Path was found with total weight: ${w}, length: ${l}`);
+    return;
+  }
+  window.alert('No path was found');
+});
+
+// -- BFS with formula
+$(document).on('click', '#js-btn-bfs-alt', function(e) {
+  clearColoring();
+  const start = $('#js-input-search-start').val();
+  const end = $('#js-input-search-end').val();
+  if (!start || !end) {
+    window.alert('Please input the start and end for the bfs');
+    return;
+  }
+
+  $('#js-input-search-start').val('');
+  $('#js-input-search-end').val('');
+
+  const startNode = getNodeById(start);
+  const endNode = getNodeById(end);
+  if (!startNode || !endNode) {
+    window.alert('The start or end node doesnt exist');
+    return;
+  }
+  window.startNode = startNode;
+  window.endNode = endNode;
+
+  const visited = [];
+  const weights = [];
+  const path = {};
+  let found = false;
+  const queue = [startNode];
+  let depth = 0;
+  while (!found) {
+    if (queue.length === 0) {
+      break;
+    }
+
+    const u = queue.pop();
+    if (u === null) {
+      depth++;
+      continue;
+    }
+    u.data('coloring', 'visited');
+    visited.push(u.id());
+
+    const adj = getAdjacentNodesAlt(u, depth)
+      .filter(vv => visited.indexOf(vv[0].id()) === -1)
+      .sort((a, b) => a[1] - b[1]);
+
+    for (const [v, weight,] of adj) {
+      path[v.id()] = u.id();
+      weights[v.id()] = weight;
+      if (v === endNode) {
+        v.data('coloring', 'path');
+        console.log('BFS found end');
+        found = true;
+        break;
+      }
+
+      queue.push(v);
+    }
+    queue.push(null);
+  }
+
+  if (found) {
+    let w = 0.8;
+    let l = 1;
+    let u = endNode.id();
+    let prev = getNodeById(u);
+    while (u !== startNode.id()) {
+      const uu = getNodeById(u);
+      uu.data('coloring', 'path');
+      if (weights[u]) {
+        w = weights[u] + w;
+      }
+      u = path[u];
+      prev = uu;
+      l += 1;
+    }
+    startNode.data('coloring', 'path');
+    window.alert(`Path was found with total FT: ${w}, length: ${l}`);
     return;
   }
   window.alert('No path was found');
